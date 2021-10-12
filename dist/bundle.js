8c8495b45a04fc81ccbac926d25ec44b8adad071
ahmed-al-farouq <mediocre23534@gmail.com>
2021-10-12 09:46:43 -0400

    Fix errors

--- dist/bundle.js
+++ dist/bundle.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _main_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./main.css */ \"./src/main.css\");\n\n\nconst h1 = document.createElement('h1');\nh1.innerHTML = '<h1>Hello</h1>';\ndocument.body.append(h1);\n\n//# sourceURL=webpack://javascript-capstone/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _main_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./main.css */ \"./src/main.css\");\n\n\ndocument.body.append(footer);\n\n//# sourceURL=webpack://javascript-capstone/./src/index.js?");
 
 /***/ })
 
