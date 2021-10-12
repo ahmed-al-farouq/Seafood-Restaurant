191e112cea0341fb2ef64e2d238f793c6e58d6f3
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-12 11:18:58 +0100

    Add footer to the project in the src/index.js file

--- src/index.js
+++ src/index.js
@@ -28,7 +28,16 @@ const createHTML = () => {
     </nav>
   </header>
   `;
-  document.body.innerHTML = header;
+  const footer = `
+    <footer>
+      <div class="logo">
+        <img src=${logo} alt="logo"/>
+      </div>
+      <div class="copyright-names">
+        <small>&#169; 2021 <a href="#">Ahmed. Al-Farouq &</a><br><a href="#">Francis Ugorji.N.</a><br>Some Rights Reserved</small>
+      </div>
+    </footer>`;
+  document.body.innerHTML = header + footer;
 };
 
 createHTML();
