bebb32e914c2e874ee5227909e52c3f6d27edc21
ahmed-al-farouq <mediocre23534@gmail.com>
2021-10-12 12:16:34 -0400

    Update the structure

--- src/index.js
+++ src/index.js
@@ -1,42 +1,8 @@
 import './main.css';
-import logo from './imgs/logo.jpg';
+import header from './components/header.js';
+import footer from './components/footer.js';
 
 const createHTML = () => {
-  const header = `
-  <header>
-    <nav>
-      <div class="logo">
-        <img src=${logo} alt="logo"/>
-      </div>
-      <ul>
-        <li>
-          <a href="#">
-            Meals(6)
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            Reservation
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            About
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  `;
-  const footer = `
-    <footer>
-      <div class="logo">
-        <img src=${logo} alt="logo"/>
-      </div>
-      <div class="copyright-names">
-        <small>&#169; 2021 <a href="#">Ahmed. Al-Farouq &</a><br><a href="#">Francis Ugorji.N.</a><br>Some Rights Reserved</small>
-      </div>
-    </footer>`;
   document.body.innerHTML = header + footer;
 };
 
