a3027e62d81dbd70a118cd8be8d32cb298d514f1
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-12 17:42:07 +0100

    Fix all linters errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './main.css';
+import header from './components/header.js';
+import footer from './components/footer.js';
 import commentPopup from './components/popup.js';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 const createHTML = () => {
-  document.body.innerHTML = commentPopup();
+  document.body.innerHTML = header + footer + commentPopup();
 };
 
 createHTML();
