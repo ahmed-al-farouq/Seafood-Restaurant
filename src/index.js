27377a7a1965a2251ef61ee9520843c8cd8e5964
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-12 17:47:53 +0100

    Edit the way the comments popup section shows in the src/index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import './main.css';
-import header from './components/header.js';
-import footer from './components/footer.js';
+
 import commentPopup from './components/popup.js';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 const createHTML = () => {
-  document.body.innerHTML = header + footer + commentPopup();
+  document.body.innerHTML = commentPopup();
 };
 
 createHTML();
