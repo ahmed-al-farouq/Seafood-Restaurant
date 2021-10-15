3917b22126def7fb40491f7afcf608c9991e2852
ahmed-al-farouq <mediocre23534@gmail.com>
2021-10-15 15:46:43 -0400

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ import logo from './imgs/logo.jpg';
 fetchData();
 const logoDivs = document.querySelectorAll('.logo');
 logoDivs.forEach((div) => {
-  div.innerHTML = `<img src="${logo}" alt="logo"/>`
-})
+  div.innerHTML = `<img src="${logo}" alt="logo"/>`;
+});
