144ced588c5d81a14466a6a8997f7732736e2751
Ahmed Al Farouq <mediocre23534@gmail.com>
2021-10-31 17:57:19 +0200

    Fix opening and closing menu issue

--- src/JSFunctions/navigatePages.js
+++ src/JSFunctions/navigatePages.js
@@ -2,9 +2,20 @@ const home = document.getElementById('meals');
 const homeSection = document.querySelector('.home-section');
 const about = document.getElementById('about');
 const aboutSection = document.getElementById('about-section');
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav');
 
 home.addEventListener('click', (e) => {
   e.preventDefault();
+  if (window.innerWidth <= 991) {
+    menuIcon.innerHTML = `
+    <i class="fas fa-bars"></i>
+    `;
+    setTimeout(() => {
+      menuIcon.style.opacity = '1';
+    }, 200);
+    nav.style.height = '0';
+  }
   homeSection.style.display = 'flex';
   aboutSection.style.display = 'none';
   home.parentElement.classList.add('active');
@@ -13,6 +24,15 @@ home.addEventListener('click', (e) => {
 
 about.addEventListener('click', (e) => {
   e.preventDefault();
+  if (window.innerWidth <= 991) {
+    menuIcon.innerHTML = `
+    <i class="fas fa-bars"></i>
+    `;
+    setTimeout(() => {
+      menuIcon.style.opacity = '1';
+    }, 200);
+    nav.style.height = '0';
+  }
   homeSection.style.display = 'none';
   aboutSection.style.display = 'flex';
   home.parentElement.classList.remove('active');
